791cb3369cf70ba6f20d901b06303ae8bc15194a
Moch Asyral Difa Alfani <asyral777@gmail.com>
2021-06-03 13:43:24 +0700

    add chat template

---
+++ resources/js/components/ChatsComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="row">
+        <div class="col-8">
+            <div class="card card-default">
+                <div class="card-header">Messages</div>
+                <div class="card-body p-0">
+                    <ul
+                        class="list-unstyled"
+                        style="height:300px; overflow-y:scroll"
+                        v-chat-scroll
+                    >
+                        <li class="p-2">
+                            <strong>Sumanta</strong>
+                            Messagees....
+                        </li>
+                    </ul>
+                </div>
+
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="Enter your message..."
+                    class="form-control"
+                />
+            </div>
+            <span class="text-muted">One User is typing...</span>
+        </div>
+
+        <div class="col-4">
+            <div class="card card-default">
+                <div class="card-header">Active Users</div>
+                <div class="card-body">
+                    <ul>
+                        <li class="py-2">
+                            Mr. X
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    }
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/chats', [App\Http\Controllers\ChatController::class, 'index'])->name('chats')->middleware('auth');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
